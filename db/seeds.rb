11832726dfc65fcbd20c24e05d4409121d4c0a8f
Luka Salevic <salevic@lu-ka.me>
2020-07-19 00:12:42 +0200

    Remove localScores from seed and generate 50 random scores

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-10.times do
+50.times do
   Score.create(username: Faker::Internet.username, wins: rand(1..500), draws: rand(1..500), losses: rand(1..500))
 end
-
-LocalScore.create(username: "Sensanaty", wins: 1500, draws: 1000, losses: 500)
